2ebf016e57834d14b8a525785517fc0b84310ea3
modood <modood@qq.com>
2015-12-13 15:41:59 +0800

    获取cookie值

---
+++ getCookie.js
@@ -0,0 +1,10 @@
+/**
+ * @description 获取cookie值
+ * @author 未知
+ */
+
+function getCookie(name) {
+    var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+    if (arr != null) return unescape(arr[2]);
+    return null
+}
