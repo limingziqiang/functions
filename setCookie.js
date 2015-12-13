2c87b307b7690c5a509d761df184669eed237a62
modood <modood@qq.com>
2015-12-13 14:32:02 +0800

    设置cookie值

---
+++ setCookie.js
@@ -0,0 +1,14 @@
+/**
+ * @description 设置cookie值
+ * @author 未知
+ */
+
+function setCookie(name, value, Hours) {
+    var d = new Date();
+    var offset = 8;
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    var nd = utc + (3600000 * offset);
+    var exp = new Date(nd);
+    exp.setTime(exp.getTime() + Hours * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";path=/;expires=" + exp.toGMTString() + ";domain=360doc.com;"
+}
