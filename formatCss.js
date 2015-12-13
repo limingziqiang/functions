7b1aff01def0aa7d3c2b3b74826c0bc96dec88c1
modood <modood@qq.com>
2015-12-13 15:41:45 +0800

    格式化CSS样式代码

---
+++ formatCss.js
@@ -0,0 +1,14 @@
+/**
+ * @description 格式化CSS样式代码
+ * @author 未知
+ */
+
+function formatCss(s){//格式化代码
+    s = s.replace(/\s*([\{\}\:\;\,])\s*/g, "$1");
+    s = s.replace(/;\s*;/g, ";"); //清除连续分号
+    s = s.replace(/\,[\s\.\#\d]*{/g, "{");
+    s = s.replace(/([^\s])\{([^\s])/g, "$1 {\n\t$2");
+    s = s.replace(/([^\s])\}([^\n]*)/g, "$1\n}\n$2");
+    s = s.replace(/([^\s]);([^\s\}])/g, "$1;\n\t$2");
+    return s;
+}
