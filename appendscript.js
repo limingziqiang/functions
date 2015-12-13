50472c32d87ab60678d7572cdc30b27be88e58d9
modood <modood@qq.com>
2015-12-13 15:37:57 +0800

    动态加载脚本文件

---
+++ appendscript.js
@@ -0,0 +1,37 @@
+/**
+ * @description 动态加载脚本文件
+ * @author 未知
+ */
+
+function appendscript(src, text, reload, charset) {
+    var id = hash(src + text);
+    if(!reload && in_array(id, evalscripts)) return;
+    if(reload && $(id)) {
+        $(id).parentNode.removeChild($(id));
+    }
+ 
+    evalscripts.push(id);
+    var scriptNode = document.createElement("script");
+    scriptNode.type = "text/javascript";
+    scriptNode.id = id;
+    scriptNode.charset = charset ? charset : (BROWSER.firefox ? document.characterSet : document.charset);
+    try {
+        if(src) {
+            scriptNode.src = src;
+            scriptNode.onloadDone = false;
+            scriptNode.onload = function () {
+                scriptNode.onloadDone = true;
+                JSLOADED[src] = 1;
+             };
+             scriptNode.onreadystatechange = function () {
+                 if((scriptNode.readyState == 'loaded' || scriptNode.readyState == 'complete') && !scriptNode.onloadDone) {
+                    scriptNode.onloadDone = true;
+                    JSLOADED[src] = 1;
+                }
+             };
+        } else if(text){
+            scriptNode.text = text;
+        }
+        document.getElementsByTagName('head')[0].appendChild(scriptNode);
+    } catch(e) {}
+}
