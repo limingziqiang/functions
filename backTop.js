bc0fdc96e4dff0b69d6ecf313eb73f850e98ac5d
modood <modood@qq.com>
2015-12-13 15:38:12 +0800

    返回顶部的通用方法

---
+++ backTop.js
@@ -0,0 +1,25 @@
+/**
+ * @description 返回顶部的通用方法
+ * @author 未知
+ */
+
+function backTop(btnId) {
+    var btn = document.getElementById(btnId);
+    var d = document.documentElement;
+    var b = document.body;
+    window.onscroll = set;
+    btn.style.display = "none";
+    btn.onclick = function() {
+        btn.style.display = "none";
+        window.onscroll = null;
+        this.timer = setInterval(function() {
+            d.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
+            b.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
+            if ((d.scrollTop + b.scrollTop) == 0) clearInterval(btn.timer, window.onscroll = set);
+            }, 10);
+    };
+    function set() {
+        btn.style.display = (d.scrollTop + b.scrollTop > 100) ? 'block': "none"
+    }
+};
+backTop('goTop');
