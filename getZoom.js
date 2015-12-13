b45292557c9d570d40aaef1e1b3581e668026a28
modood <modood@qq.com>
2015-12-13 15:46:31 +0800

    获取移动设备最大化大小

---
+++ getZoom.js
@@ -0,0 +1,18 @@
+/**
+ * @description 获取移动设备最大化大小
+ * @author 未知
+ */
+
+function getZoom(){
+    var screenWidth = (Math.abs(window.orientation) === 90) ? Math.max(screen.height, screen.width) : Math.min(screen.height, screen.width);
+    if(this.isAndroidMobileDevice() && !this.isNewChromeOnAndroid()){
+        screenWidth = screenWidth/window.devicePixelRatio;
+    }
+    var FixViewPortsExperiment = rendererModel.runningExperiments.FixViewport || rendererModel.runningExperiments.fixviewport;
+    var FixViewPortsExperimentRunning = FixViewPortsExperiment && (FixViewPortsExperiment === "New" || FixViewPortsExperiment === "new");
+    if(FixViewPortsExperimentRunning){
+        return screenWidth / window.innerWidth;
+    }else{
+        return screenWidth / document.body.offsetWidth;
+    }
+}
