e455b701db4c841cf30d15880e661a6a0366e7bc
modood <modood@qq.com>
2015-12-13 15:45:25 +0800

    获取移动设备屏幕宽度

---
+++ getScreenWidth.js
@@ -0,0 +1,16 @@
+/**
+ * @description 获取移动设备屏幕宽度
+ * @author 未知
+ */
+
+function getScreenWidth(){
+    var smallerSide = Math.min(screen.width, screen.height);
+    var fixViewPortsExperiment = rendererModel.runningExperiments.FixViewport || rendererModel.runningExperiments.fixviewport;
+    var fixViewPortsExperimentRunning = fixViewPortsExperiment && (fixViewPortsExperiment.toLowerCase() === "new");
+    if(fixViewPortsExperiment){
+        if(this.isAndroidMobileDevice() && !this.isNewChromeOnAndroid()){
+            smallerSide = smallerSide/window.devicePixelRatio;
+        }
+    }
+    return smallerSide;
+}
