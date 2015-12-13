a78fb9c43db55bc5eb0f44a2847043a9bd0efec6
modood <modood@qq.com>
2015-12-13 14:17:03 +0800

    字符串长度截取

---
+++ cutstr.js
@@ -0,0 +1,25 @@
+/**
+ * @description 字符串长度截取
+ * @author 未知
+ */ 
+
+function cutstr(str, len) {
+    var temp,
+        icount = 0,
+        patrn = /[^\x00-\xff]/，
+        strre = "";
+    for (var i = 0; i < str.length; i++) {
+        if (icount < len - 1) {
+            temp = str.substr(i, 1);
+                if (patrn.exec(temp) == null) {
+                   icount = icount + 1
+            } else {
+                icount = icount + 2
+            }
+            strre += temp
+            } else {
+            break;
+        }
+    }
+    return strre + "..."
+}
