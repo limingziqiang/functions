8c18aefbf791888948062e248af73e16901f5aa5
modood <modood@qq.com>
2015-12-13 15:41:18 +0800

    返回脚本内容

---
+++ evalscript.js
@@ -0,0 +1,23 @@
+/**
+ * @description 返回脚本内容
+ * @author 未知
+ */
+
+function evalscript(s) {
+    if(s.indexOf('<script') == -1) return s;
+    var p = /<script[^\>]*?>([^\x00]*?)<\/script>/ig;
+    var arr = [];
+    while(arr = p.exec(s)) {
+        var p1 = /<script[^\>]*?src=\"([^\>]*?)\"[^\>]*?(reload=\"1\")?(?:charset=\"([\w\-]+?)\")?><\/script>/i;
+        var arr1 = [];
+        arr1 = p1.exec(arr[0]);
+        if(arr1) {
+            appendscript(arr1[1], '', arr1[2], arr1[3]);
+        } else {
+            p1 = /<script(.*?)>([^\x00]+?)<\/script>/i;
+            arr1 = p1.exec(arr[0]);
+            appendscript('', arr1[2], arr1[1].indexOf('reload=') != -1);
+        }
+    }
+    return s;
+}
