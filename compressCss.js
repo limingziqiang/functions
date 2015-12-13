6a1f23fb572f68c91d370a1783dcdaf263feb7f9
modood <modood@qq.com>
2015-12-13 15:39:16 +0800

    压缩CSS样式代码

---
+++ compressCss.js
@@ -0,0 +1,13 @@
+/**
+ * @description 压缩CSS样式代码
+ * @author 未知
+ */
+
+function compressCss (s) {//压缩代码
+    s = s.replace(/\/\*(.|\n)*?\*\//g, ""); //删除注释
+    s = s.replace(/\s*([\{\}\:\;\,])\s*/g, "$1");
+    s = s.replace(/\,[\s\.\#\d]*\{/g, "{"); //容错处理
+    s = s.replace(/;\s*;/g, ";"); //清除连续分号
+    s = s.match(/^\s*(\S+(\s+\S+)*)\s*$/); //去掉首尾空白
+    return (s == null) ? "" : s[1];
+}
