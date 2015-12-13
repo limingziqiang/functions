44aec9659d87a914b9e6a63d762bebe3cd472bf9
modood <modood@qq.com>
2015-12-13 15:42:36 +0800

    获取移动设备初始化大小

---
+++ getInitZoom.js
@@ -0,0 +1,15 @@
+/**
+ * @description 获取移动设备初始化大小
+ * @author 未知
+ */
+
+function getInitZoom(){
+    if(!this._initZoom){
+        var screenWidth = Math.min(screen.height, screen.width);
+        if(this.isAndroidMobileDevice() && !this.isNewChromeOnAndroid()){
+            screenWidth = screenWidth/window.devicePixelRatio;
+        }
+            this._initZoom = screenWidth /document.body.offsetWidth;
+        }
+    return this._initZoom;
+}
