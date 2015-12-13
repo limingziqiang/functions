f897de77b68030a0fda6bcf5d53ef0a509e1f4df
modood <modood@qq.com>
2015-12-13 15:49:07 +0800

    判断是否为网址

---
+++ isURL.js
@@ -0,0 +1,13 @@
+/**
+ * @description 判断是否为网址
+ * @author 未知
+ */
+
+function isURL(strUrl) {
+    var regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|edu|gov|int|mil|net|org|biz|info|name|museum|asia|coop|aero|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i
+    if (regular.test(strUrl)) {
+        return true;
+    }else {
+        return false;
+    }
+}
