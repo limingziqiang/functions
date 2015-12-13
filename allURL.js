b66093f42319f5345b958f5359beb178df7c327d
modood <modood@qq.com>
2015-12-13 15:37:38 +0800

    提取页面代码中所有网址

---
+++ allURL.js
@@ -0,0 +1,7 @@
+/**
+ * @description 提取页面代码中所有网址
+ * @author 未知
+ */
+
+var aa = document.documentElement.outerHTML.match(/(url\(|src=|href=)[\"\']*([^\"\'\(\)\<\>\[\] ]+)[\"\'\)]*|(http:\/\/[\w\-\.]+[^\"\'\(\)\<\>\[\] ]+)/ig).join("\r\n").replace(/^(src=|href=|url\()[\"\']*|[\"\'\>\) ]*$/igm,"");
+alert(aa);
