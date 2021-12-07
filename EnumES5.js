c33eb07276e16a7793c0ef8eb1a513f14bbf0157
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2021-12-07 14:48:56 +0800

    Create EnumES5.js

---
+++ EnumES5.js
@@ -0,0 +1,11 @@
+var Role;
+
+(function (Role) {
+
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+
+    Role[Role["VISITER"] = 1] = "VISITER";
+
+    Role[Role["EMPLOYEE"] = 2] = "EMPLOYEE";
+
+})(Role || (Role = {}));
