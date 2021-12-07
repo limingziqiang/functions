82e599ee5626cfc014ec23a1f5470b931984e464
²¹带你去旅行♬ ♫♬ <limingziqiang@126.com>
2021-12-07 15:02:18 +0800

    Create createEnumKeyEqualsValue.js

---
+++ createEnumKeyEqualsValue.js
@@ -0,0 +1,10 @@
+function createEnum(values) {
+  const enumObject = {};
+  for (const val of values) {
+    enumObject[val] = val;
+  }
+  return Object.freeze(enumObject);
+}
+
+// { Up: 'Up', Down: 'Down', Left: 'Left', Right: 'Right' }
+createEnum(['Up', 'Down', 'Left', 'Right']);
